487c4e637ad5b7cca1ec9a965c78d9cc6787cd8c
Christopher Tham <chriswt91@gmail.com>
2016-09-24 00:11:26 -0700

    Implement cartoDB base map + backup for load failure

--- map.js
+++ map.js
@@ -1,12 +1,17 @@
 var map = new L.Map('map', {zoomControl: false})
-          .setView([37.7749, -122.4312], 12);
+          .setView([37.7749, -122.4312], 12.5);
 
-L.tileLayer(
-  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-}).addTo(map);
+var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',{ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
 
-map._initPathRoot()    
+// Backup Map For Failed BaseMap Loading
 
+// L.tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-dark/{z}/{x}/{y}.png', {
+//   attribution: 'dark_cartodb'
+// }).addTo(map);
+
+map.addLayer(layer);
+
+map._initPathRoot();
 
 // Disable zoom options on map
 map.touchZoom.disable();
@@ -15,3 +20,6 @@ map.scrollWheelZoom.disable();
 map.boxZoom.disable();
 map.keyboard.disable();
 map.dragging.disable();
+
+// Pick up SVG from map object
+// var svg = d3.select('#map').select('svg');
